e318593dc4b5f2edc633c65065478ec8c6e0db82
BimaBizz <mahendrabima278@gmail.com>
2022-12-08 09:59:08 +0700

    Portofolio web ke 2

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
